0da88a369544acf5aa157a06dbd7196cce72fbb0
Ganesh KT <ganesh0211@gmail.com>
2021-07-25 18:40:22 +0100

    Create CombinationAndPermutation.java

---
+++ String/CombinationAndPermutation.java
@@ -0,0 +1,25 @@
+/***********************
+*
+* Author: Ganesh
+*
+***********************/
+
+public class CombinationAndPermutation{
+
+private static void allStringCombinationSameSize(String str,String ss){
+        if(str.length() == 0){
+          System.out.println(ss);
+          return;
+        }
+        boolean cused[] = new boolean[26];
+        for(int i=0;i<str.length();i++){
+          char c = str.charAt(i);
+          String otherContent = str.substring(0,i)+str.substring(i+1);
+          if(cused[c-'a'] == false){
+            allStringCombination(otherContent,ss+c);
+            cused[c-'a']=true;
+          }
+          
+        }
+    }
+    }
